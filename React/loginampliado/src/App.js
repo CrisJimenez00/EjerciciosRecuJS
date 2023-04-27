dafcbb8fac45e0fe4d606b94673bc6ff37954f22
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2023-04-27 12:45:02 +0200

    Lo he pasado pero no hace nada(hay que mirarlo)

--- React/loginampliado/src/App.js
+++ React/loginampliado/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+/*import { Component } from 'react';
 import {Button} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from './componentes/Menu';
@@ -56,6 +56,124 @@ class App extends Component {
       </div>
     );
     }
+}
+
+export default App;*/
+/*import { Component } from 'react';
+import {Button} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from './componentes/Menu';
+import AppLogin from './componentes/AppLogin';
+import {PHPLOGIN} from './componentes/Datos'
+import axios from 'axios';
+import {useState} from "react";
+
+function App() {
+    const [activa, setActiva] = useState(false);
+    const [correcto, setCorrecto] = useState(true);
+    const [error, setError] = useState(false);
+
+    function comprobar(usuario, clave) {
+        axios.post(PHPLOGIN, JSON.stringify({usuario: usuario, clave: clave})).then(r => {
+            if (r.data.mensaje === "Acceso correcto") {
+                setActiva(true);
+                setCorrecto(true);
+                setError(false);
+            }
+            if (r.data.mensaje === "Acceso denegado") {
+                setCorrecto(false);
+                setError(false);
+            }
+            else {
+                setError(true)
+            }
+        });
+    }
+
+    const obj = activa ? <Menu/> : <AppLogin error={error} correcto={correcto} f={comprobar}/>
+    return (<>
+            {obj}
+        </>)
+}
+
+export default App;*/
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from './componentes/Menu';
+import AppLogin from './componentes/AppLogin';
+import {PHPLOGIN} from './componentes/Datos'
+import axios from 'axios';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuItem: "DOS",
+      //logged:false,
+
+    }
+  }
+
+  changeMenu(item) {
+    this.setState({ menuItem: item })
+  }
+
+  /*userLogin(telefono, password) {
+    console.log(telefono + " " + password)
+    axios.post(PHPLOGIN, JSON.stringify({
+      telefono: telefono,
+      password: password
+    })).then(res => {
+      if (res.data.usuario != undefined) {
+        this.setState({ logged: true })
+      }
+      this.setState({ info: res.data.mensaje })
+    }
+    );
+  }
+
+  setInfo(i) {
+    this.setState({ info: i })
+  }*/
+
+  setTitulo(boton, t) {
+    let copia = this.state.titulo;
+    copia[boton] = t;
+    this.setState({ titulo: copia });
+  }
+
+  //Método que comprueba 
+  userLogin(telefono, password) {
+    axios.post(PHPLOGIN,
+      JSON.stringify({
+        telefono: telefono,
+        password: password
+      })
+    ).then(res => {
+      if (res.data.usuario != undefined) {
+        //Se conecta:
+        this.setState({ logged: true })
+      }
+      this.setState({ info: res.data.mensaje })
+    }
+    )
+  }
+
+  setInfo(i) {
+
+    this.setState({ info: i })
+  }
+  //Al poner las funciones como flecha se auto-bindean
+  render() {
+    return (
+      <div className="App">
+        <Menu menuItem={this.state.menuItem} changeMenu={(item) => this.changeMenu(item)} />
+        <AppLogin usuarioLogin={(telefono, password)=>this.userLogin(telefono,password)}></AppLogin>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ React/loginampliado/src/componentes/Uno.js
@@ -0,0 +1,45 @@
+
+import { Button } from "reactstrap";
+export default function Uno() {
+    return(
+        <Button>MIAAAAU</Button>
+    )
+}
+    
+import { useState } from "react";
+import { Button, Form, Input, FormGroup, Label, Row, Col } from "reactstrap";
+export default function Uno(props) {
+    //hooks
+    const [texto, setTexto] = useState("");
+    const handleChange = (event) => {
+        if (event.target.name == "texto") {
+            setTexto(event.target.value) //actualiza el valor en el hook
+        }
+    }
+    const handleSubmit = () => {
+        if (texto != "") {
+            props.setTitulo(texto) //actualiza el estado
+        }
+    }
+    return (
+        <Row>
+            <Col sm="4"></Col>
+            <Col sm="4">
+                <Form>
+                    <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+                        <Label className="me-sm-2">Añade un título</Label>
+                        <Input
+                            id="texto"
+                            name="texto"
+                            placeholder="Introduzca un título"
+                            onChange={handleChange} />
+                    </FormGroup>
+                    <br />
+                    <FormGroup>
+                        <Button onClick={handleSubmit}>Añadir titulo</Button>
+                    </FormGroup>
+                </Form>
+            </Col>
+        </Row >
+    )
+}
